c1a44358e3303c285ef20c0ec93d2a5c43907618
Marcel Herd <me@marcelherd.com>
2024-02-06 16:54:42 +0100

    Updated Discord text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@ import Button from 'primevue/button';
   <main>
     <p>Built by Xiaoxiao from Ouroboros of Terraindom s66.</p>
     <a href="https://discord.gg/nbhE9vjdb8"
-      ><Button label="Join Sanctum Discord" icon="pi pi-discord" size="small"
+      ><Button label="Join our Discord" icon="pi pi-discord" size="small"
     /></a>
   </main>
 </template>
